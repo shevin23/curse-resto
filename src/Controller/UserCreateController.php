c179f3e832df1d84330819fbc2c4e57f7c0bb052
Anmol Vishvas <anmol23vishvas@gmail.com>
2025-05-08 22:38:17 +0400

    Implement mailer

--- src/Controller/UserCreateController.php
+++ src/Controller/UserCreateController.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCreateController extends AbstractController
@@ -15,6 +17,7 @@ class UserCreateController extends AbstractController
         Request $request,
         UserPasswordHasherInterface $passwordHasher,
         EntityManagerInterface $entityManager,
+        MailerInterface $mailer,
     ): Response {
         $data = json_decode($request->getContent(), true);
 
@@ -31,6 +34,13 @@ class UserCreateController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
 
+        $email = (new Email())
+            ->from('shevinlingiah@gmail.com')
+            ->to($user->getEmail())
+            ->subject('Welcome to the Undead Feast—You’re One of Us Now!')
+            ->text("Greetings, lost soul...\n\nThe gates of the Zombie & Mummy Resto have creaked open for you, and there’s no turning back! Your registration is confirmed—your fate is sealed.\n\nPrepare for a feast like no other, where the undead roam and ancient mummies whisper secrets of the underworld.\n\nKeep an eye out… We may be watching.\n\nSee you soon in the land of the living… or not.\n\n🕯️ The Undead Resto Team 🕯️")
+        ;
+        $mailer->send($email);
 
         return $this->json([
             'message' => 'User registered successfully',
